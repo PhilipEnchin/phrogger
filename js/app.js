897b6de43b623499c0419599bc626340c691c388
Philip Enchin <Philip@Ench.in>
2014-11-20 11:39:37 -0500

    Player moves around board

--- js/app.js
+++ js/app.js
@@ -1,46 +1,70 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+	this.sprite = 'images/enemy-bug.png'; //The image used for enemies
+	this.x = 0; //| X and Y     |
+	this.y = 0; //| coordinates |
+	this.speed = 5; //Speed, in pixels per second
+};
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-}
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-}
+	this.x += this.speed * dt;
+};
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// The player
+var Player = function() {
+	this.sprite = 'images/char-boy.png';
+	this.x = 0;
+	this.y = 395;
+};
+// Update the player's position
+Player.prototype.update = function() {
+	
+};
+// Draw the player on the screen
+Player.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// Handle keyboard input for the movement of the player
+Player.prototype.handleInput = function(keyString) {
+	if (keyString === 'left') {
+		this.x -= 101;
+	} else if (keyString === 'right') {
+		this.x += 101;
+	} else if (keyString === 'up') {
+		this.y -= 83;
+	} else if (keyString === 'down') {
+		this.y += 83;
+	}
+};
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+var allEnemies = []; //Array of active enemies
+var retiredEnemies = []; //Array of enemies not in use, recycled for later
+var player = new Player(); //Our hero!
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+document.addEventListener('keydown', function(e) {
+	var keyCode = e.keyCode;
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
+	// console.log(e.keyCode + ": " + allowedKeys[e.keyCode]);
+	switch (keyCode) {
+		case 37:	// |	37-40:
+		case 38:	// |	Arrow keys
+		case 39:	// V	Handled by player
+		case 40:	player.handleInput(allowedKeys[keyCode]);
+					break;
+	}
+	
 });
