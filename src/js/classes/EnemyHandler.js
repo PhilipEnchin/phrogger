90d825ee54e94b25322559643222e29b75b1b43e
Philip Enchin <Philip@Ench.in>
2020-02-26 10:20:07 -0500

    Clean up EnemyHandler constructor

--- src/js/classes/EnemyHandler.js
+++ src/js/classes/EnemyHandler.js
@@ -25,72 +25,23 @@ const packageEnemyWithEntryAndExitTimes = enemy => {
   return { enemy, entryTimes, exitTimes };
 };
 
-/**
- * A system for dealing with the evil bugs! Takes care of initializing the bugs,
- * making sure they're retired at the right time, caching collision times, and
- * recycling the Enemy objects for reuse.
- */
+/* A system for dealing with the evil bugs! Takes care of initializing the bugs,
+   making sure they're retired at the right time, caching collision times, and
+   recycling the Enemy objects for reuse. */
 class EnemyHandler {
   constructor(ctx) {
-    /**
-     * An array to hold objects that contain an Enemy object that's currently
-     * being updated and rendered, and a retire time.
-     * @type {Array.<Object.<string, Enemy | number>>}
-     */
     this.activeEnemies = [];
-    /**
-     * An array to hold objects that contain an Enemy object that's being held
-     * for later reuse, and its former retire time.
-     * @type {Array.<Object.<string, Enemy | number>>}
-     */
     this.retiredEnemies = [];
-    /**
-     * Average time between enemy spawns.
-     * @type {number}
-     */
     this.spawnInterval = null;
-    /**
-     * Amount by which this.timeUntilSpawn will vary when regenerated, in terms
-     * of a fraction of this.spawnInterval. (So a variance of 1.0 will result in
-     * this.timeUntilSpawn values of zero to 2*this.spawnInterval.)
-     * @type {number}
-     */
     this.spawnVariance = null;
-    /** @type {number} */ this.timeUntilSpawn = 0;
-    /**
-     * Whether or not enemies should update
-     * @type {boolean}
-     */
-    this.moveable = true;
-    /** @type {boolean} */ this.hidden = false;
-    /**
-     * An object full of enemies organized by row, and packaged with collision
-     * times.
-     * @type {Object.<number, Array.<Object.<string, Enemy | Array<number>>>>}
-     */
+    this.timeUntilSpawn = 0;
+    this.moveable = true; // Whether enemies should be moving abuot
+    this.hidden = false;
     this.activeEnemiesByRow = {};
-    /**
-     * Last reported location of the played. Used when a new collision time is
-     * required.
-     * @type {Object.<string, number>}
-     */
-    this.potentialCollisionLocation = { column: null, rowIndex: null };
-    /**
-     * Used to keep a running total of how much time the game is paused, then,
-     * when unpaused, used to adjust collision-related and retirement times.
-     * @type {number}
-     */
-    this.timePaused = 0;
-    /**
-     * X coordinate where an enemy is spawned (just off left of screen)
-     * @type {number}
-     */
-    this.spawnX = null;
-    /**
-     * X coordinate where an enemy is retired (just off right of screen)
-     * @type {number}
-     */
-    this.retireX = null;
+    this.potentialCollisionLocation = { column: null, rowIndex: null }; // Player location
+    this.timePaused = 0; // Running total of time paused to adjust collisions after unpausing
+    this.spawnX = null; // Enemy spawn location
+    this.retireX = null; // Enemy retire location
 
     this.ctx = ctx;
   }
